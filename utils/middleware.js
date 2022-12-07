7540da3a6ab27f19a1db648ed7388c91d0a937da
karppienkingi <mirka.vaananen@gmail.com>
2022-12-07 14:27:27 +0200

    ex 20

--- utils/middleware.js
+++ utils/middleware.js
@@ -20,14 +20,25 @@ const errorHandler = (error, request, response, next) => {
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
   } else if (error.name === 'JsonWebTokenError') {
-    return response.status(401).json({error: 'token missing or invalid'})
+    return response.status(401).json({ error: 'token missing or invalid' })
   }
 
   next(error)
 }
 
+const tokenExtractor = async (request, response, next) => {
+  console.log('täällä')
+  const authorization = await request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    request.token = authorization.substring(7)
+    console.log(request.token)
+  }
+  next()
+}
+
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  tokenExtractor
 }
